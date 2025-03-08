de0622f8ecb7d078c33befe0f7e47bcb052ffaaa
boris <henrinyemeck@gmail.com>
2025-03-08 07:00:52 +0100

    setup react-router-dom

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='bg-[#18191b] min-h-screen w-full'>
+            <div className="bg-[#212223] shadow-md">
+                <div className="w-[93%] m-auto py-3">
+                    <div className="flex justify-between items-center">
+                        <div className="w-[80px] h-[48px]">
+                            <img src="https://static.canva.com/web/images/8439b51bb7a19f6e65ce1064bc37c197.svg" alt="" className='w-full h-full' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
