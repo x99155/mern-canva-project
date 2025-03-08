8cbd8fbe2ee00e4ae15c2accc95fa0131bc7b8bd
boris <henrinyemeck@gmail.com>
2025-03-08 07:17:13 +0100

    design homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,7 +9,20 @@ const Home = () => {
                         <div className="w-[80px] h-[48px]">
                             <img src="https://static.canva.com/web/images/8439b51bb7a19f6e65ce1064bc37c197.svg" alt="" className='w-full h-full' />
                         </div>
+
+                        <div className="flex gap-4">
+                            <button className="py-2 w-[80px] text-center bg-teal-700 text-white transition-all hover:bg-teal-500 rounded-[5px] font-medium cursor-pointer">Sign up</button>
+                            <button className="py-2 w-[80px] text-center bg-purple-700 text-white transition-all hover:bg-purple-500 rounded-[5px] font-medium cursor-pointer">Sign in</button>
+                        </div>
                     </div>
+                </div>
+            </div>
+
+            <div className="w-full h-full justify-center items-center p-4">
+                <div className="py-[170px] flex justify-center items-center flex-col gap-6">
+                    <h2 className='text-5xl text-[#c7c5c5] font-bold'>What you will design today ?</h2>
+                    <span className='text-[#aca9a9] text-2xl font-medium'>Canvas make it easy to create and share professional design.</span>
+                    <button className="py-2 w-[200px] text-center bg-teal-700 text-white transition-all hover:bg-teal-500 rounded-[5px] font-medium cursor-pointer">Sign up for free</button>
                 </div>
             </div>
         </div>
